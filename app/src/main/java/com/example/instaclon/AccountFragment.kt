9cafc45665e42f6b9d7cd6b3fec22c4b25fdbfd5
Junsuk Oh <a811219@gmail.com>
2019-06-04 11:53:39 +0900

    구글 로그인, 로그아웃 구현

--- app/src/main/java/com/example/instaclon/AccountFragment.kt
+++ app/src/main/java/com/example/instaclon/AccountFragment.kt
@@ -1,12 +1,18 @@
 package com.example.instaclon
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 
 class AccountFragment : Fragment() {
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,12 +20,33 @@ class AccountFragment : Fragment() {
     ): View? {
 
         setHasOptionsMenu(true)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(requireContext(), gso)
+
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.account, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut()
+            googleSignInClient.signOut()
+
+            // 로그인 화면으로 이동
+            startActivity(Intent(requireContext(), LoginActivity::class.java))
+            requireActivity().finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 
