c05e2c322ac3172a450bf803aa9773d28d470552
Junsuk Oh <a811219@gmail.com>
2019-06-10 16:56:58 +0900

    저장된 사진 불러오기

--- app/src/main/java/com/example/instaclon/CreatePostFragment.kt
+++ app/src/main/java/com/example/instaclon/CreatePostFragment.kt
@@ -1,30 +1,78 @@
 package com.example.instaclon
 
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.fragment_create_post.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class CreatePostFragment : Fragment() {
+    private val imageUri = MutableLiveData<Uri>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_create_post, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        imageUri.observe(this, Observer { uri ->
+            Glide.with(imageView)
+                .load(uri)
+                .into(imageView)
+        })
+
+        camera_button.setOnClickListener {
+            selectImage()
+        }
+    }
+
+    private fun selectImage() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+            type = "image/*"
+        }
+        if (intent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_GET)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
+        super.onActivityResult(requestCode, resultCode, intent)
+
+        if (requestCode == REQUEST_IMAGE_GET
+            && resultCode == Activity.RESULT_OK
+            && intent != null) {
+            imageUri.value = intent.data!!
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.create_post, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_send) {
+            // 이미지 업로드
+            // 업로드 결과를 DB에 작성
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        const val REQUEST_IMAGE_GET = 1
     }
 
 
