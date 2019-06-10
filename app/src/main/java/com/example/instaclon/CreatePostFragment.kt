2ca07679c189c8268f6f24804129ef4360463a27
Junsuk Oh <a811219@gmail.com>
2019-06-10 20:23:05 +0900

    사진 업로드 구현 중

--- app/src/main/java/com/example/instaclon/CreatePostFragment.kt
+++ app/src/main/java/com/example/instaclon/CreatePostFragment.kt
@@ -5,11 +5,15 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import com.bumptech.glide.Glide
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.UploadTask
 import kotlinx.android.synthetic.main.fragment_create_post.*
 
 
@@ -52,7 +56,8 @@ class CreatePostFragment : Fragment() {
 
         if (requestCode == REQUEST_IMAGE_GET
             && resultCode == Activity.RESULT_OK
-            && intent != null) {
+            && intent != null
+        ) {
             imageUri.value = intent.data!!
         }
     }
@@ -65,10 +70,23 @@ class CreatePostFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.action_send) {
             // 이미지 업로드
+            imageUri.value?.let {
+                val downloadUri = uploadImage(it).task.result
+                Log.d("Log", "${downloadUri.toString()}")
+            }
+
             // 업로드 결과를 DB에 작성
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    fun uploadImage(uri: Uri): UploadTask.TaskSnapshot {
+        val stream = requireContext().contentResolver.openInputStream(uri)
+
+        val ref = FirebaseStorage.getInstance().reference
+            .child("images/${System.currentTimeMillis()}.jpg")
+        return Tasks.await(ref.putStream(stream!!))
     }
 
     companion object {
